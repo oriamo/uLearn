fe156e62e3a18a59aa9047d5ab84e622b8ef0893
oluwadarasimi <Adewoledarasimi25@gmail.com>
2023-08-28 22:49:17 -0400

    refactored code for images and banners

---
+++ lib/pages/application/home/controller/home_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$homeScreenBannerDotsHash() =>
+    r'9df4295db93646b440282ede18e874f6480dfcd7';
+
+/// See also [HomeScreenBannerDots].
+@ProviderFor(HomeScreenBannerDots)
+final homeScreenBannerDotsProvider =
+    AutoDisposeNotifierProvider<HomeScreenBannerDots, int>.internal(
+  HomeScreenBannerDots.new,
+  name: r'homeScreenBannerDotsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$homeScreenBannerDotsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$HomeScreenBannerDots = AutoDisposeNotifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
